287cffa772519ec9edf8cc903c846a60db8db8c0
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2024-08-17 18:20:04 -0500

    Complete Chapter 1

---
+++ Ch01/01_01/dist/model.js
@@ -0,0 +1,22 @@
+var displayName = "Jess's standing desk";
+var inventoryType = "furniture";
+var trackingNumber = "FD123455";
+var createDate = new Date();
+var originalCost = 425;
+// function getInventoryItem(trackingNumber: string): object {
+//     return null;
+// }
+// function getInventoryItem(trackingNumber: string): void {
+//     
+// }
+function getInventoryItem(trackingNumber) {
+    return null;
+}
+function saveInventoryItem(item) {
+}
+var inventoryItem = getInventoryItem(trackingNumber);
+inventoryItem.createDate = new Date();
+var blablabla = 34;
+blablabla = 'werwer';
+var sniff = inventoryItem;
+saveInventoryItem(inventoryItem);
