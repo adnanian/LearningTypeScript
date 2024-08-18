6cdf94efa044ed4e91d1c677bec7f1138f1cce23
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2024-08-18 07:11:37 -0500

    Complete lesson

--- Ch02/02_01/src/model.ts
+++ Ch02/02_01/src/model.ts
@@ -4,17 +4,29 @@ let trackingNumber: string = "FD123455";
 let createDate: Date = new Date();
 let originalCost = 425;
 
-function getInventoryItem(trackingNumber: string): {
+// function getInventoryItem(trackingNumber: string): {
+//     displayName: string;
+//     inventoryType: string;
+//     trackingNumber: string;
+//     createDate: Date;
+//     originalCost: number;
+// } {
+//     return null;
+// }
+
+interface InventoryItem {
     displayName: string;
     inventoryType: string;
     trackingNumber: string;
     createDate: Date;
     originalCost: number;
-} {
+}
+
+function getInventoryItem(trackingNumber: string): InventoryItem {
     return null;
 }
 
-function saveInventoryItem(item) {
+function saveInventoryItem(item: InventoryItem) {
 
 }
 
